fa0d5ea0e444be238c3ab3245cf013b6ef782673
Mahmudul Hassan <mdhassan6767@gmail.com>
2020-09-05 17:52:58 +0600

    master commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_IJfXzMqPB', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '705ks2icetarvtdekfvd5ik6dc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
